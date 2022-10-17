8f92d79c059a6b50f8fd9035827d36138adc5c8c
mahdirahmani1376 <rahmanimahdi16@gmail.com>
2022-10-17 12:11:54 +0330

    article change

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -9,11 +9,25 @@ class Article extends Model
 {
     use HasFactory;
 
+    protected $with = ['category','tags'];
+
     public function category(){
         return $this->belongsTo(Category::class);
     }
 
     public function tags(){
-        return $this->belongsToMany(Tags::class);
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function authors(){
+        return $this->belongsTo(User::class,'user_id');
+    }
+
+    public function scopeFilter($query, array $filters){
+        $query->when($filters['category'] ?? false, fn($query , $category) =>
+            $query->wherehas('category', fn($query) =>
+                $query->where('name',$category)
+            )
+        );
     }
 }
